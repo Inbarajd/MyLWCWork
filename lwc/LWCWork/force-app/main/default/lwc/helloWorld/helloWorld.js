cc85c22b5c4d5b11d5b256835ab485213ed358b2
dumCodder <Inbarajd@users.noreply.github.com>
2019-08-11 18:34:52 -0400

    update

--- lwc/LWCWork/force-app/main/default/lwc/helloWorld/helloWorld.js
+++ lwc/LWCWork/force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -1,3 +1,20 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
-export default class HelloWorld extends LightningElement {}
+export default class HelloWorld extends LightningElement {
+    @track greetings = "Inbaraj";
+    _title = this.greetings &'hello Bla Bla Bla....!!!!!!';
+
+    eventHandle(event){
+        this.greetings = event.target.value;
+    }
+    dispGreetings(event){
+        const evt = new ShowToastEvent({
+            title: this._title,
+            message: 'Hello,{greetings}',
+            variant: 'success',
+            mode: 'dismissable'
+        });
+        this.dispatchEvent(evt);
+    }
+}
